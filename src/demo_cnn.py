81992ea58fa2cad88f9dd99330b162d01a3154d4
zanderalbaz <119557980+zanderalbaz@users.noreply.github.com>
2024-05-03 21:09:16 -0600

    Update demo_cnn.py

--- src/demo_cnn.py
+++ src/demo_cnn.py
@@ -1,5 +1,5 @@
 # THIS CNN WAS USED FOR OUR FIRST DEMONSTRATION
-# THIS CNN
+# THIS CNN IS NOT OUR FINAL CNN
 
 
 import pandas as pd
@@ -11,13 +11,13 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, Flatten, Dense, Dropout, Reshape, BatchNormalization, MaxPooling2D
 from tensorflow.keras.callbacks import EarlyStopping
 
-def normalize_data(data):
+def normalize_data(data): #min max scaling
     max_val = np.max(data)
     min_val = np.min(data)
     normalized_data = 2 * ((data - min_val) / (max_val - min_val)) - 1
     return normalized_data
 
-def processSet(pb_set):
+def processSet(pb_set): #process points from csv
     pb_set = pb_set.replace('[(', '')
     pb_set = pb_set.replace(')]', '')
     return pb_set.split('), (')
@@ -25,25 +25,25 @@ def processSet(pb_set):
 def processPoints(p_list):
     points = []
     for ps in p_list:
-        points.append(processPoints_(ps))
+        points.append(processPoints_(ps)) #helper process function
     return np.asarray(points)
 
 def processPoints_(ps):
     xs = []
     ys = []
     for point in ps:
-        p = point.split(', ')
+        p = point.split(', ')#split into x and y 
         x = p[0]
         xs.append(int(float(x))) #convert to float then int to catch points that were not quantized
         y = p[1]
         ys.append(int(float(y))) 
-        
+    #return tensor of points
     points = []
     points.append(np.array(xs))
     points.append(np.array(ys))
     return np.asarray(points)
 
-def augment_data(data, noise_std=0.05, transform_scale=0.1):
+def augment_data(data, noise_std=0.05, transform_scale=0.1): #generate more data
     augmented_data = []
     for point_batches, label in data:
         augmented_point_batches = []
@@ -51,9 +51,13 @@ def augment_data(data, noise_std=0.05, transform_scale=0.1):
             augmented_point_set = []
             for _ in range(2):  # augment each point set 2 times
                 point_set_reshaped = point_set.reshape((500, 2))  
+                #add random noise to our data
                 noisy_point_set = point_set_reshaped + np.random.normal(loc=0.0, scale=noise_std, size=point_set_reshaped.shape)
+                #shift all of our datapoints by a small factor
                 transformed_point_set = point_set_reshaped * (1 + np.random.uniform(-transform_scale, transform_scale, size=point_set_reshaped.shape))
-                
+
+
+                #add both augmehtations to our point set
                 augmented_point_set.append(normalize_data(noisy_point_set))  
                 augmented_point_set.append(normalize_data(transformed_point_set)) 
             
@@ -61,29 +65,32 @@ def augment_data(data, noise_std=0.05, transform_scale=0.1):
     
     return augmented_data
 
-
+#read data from csv
 points = pd.read_csv('labeled_points.csv', sep='\t', header=None, names=["point_batch1", "point_batch2", "point_batch3", "point_batch4", "point_batch5", "Room+Direction"])
 
 labeled_data = []
 for i in range(len(points)): 
+    #get each point batch (5 collected each interval)
     pb_set1 = points['point_batch1'][i]
     pb_set2 = points['point_batch2'][i]
     pb_set3 = points['point_batch3'][i]
     pb_set4 = points['point_batch4'][i]
     pb_set5 = points['point_batch5'][i]
 
+    #process sets
     points1 = processSet(pb_set1)
     points2 = processSet(pb_set2)
     points3 = processSet(pb_set3)
     points4 = processSet(pb_set4)
     points5 = processSet(pb_set5)
 
+    #process combined point batches
     points_list = [points1, points2, points3, points4, points5]
     point_batches = processPoints(points_list)
     label = points["Room+Direction"][i]
 
     labeled_data.append((point_batches, label))
-
+#one-hot encode labels
 labels = [label for _, label in labeled_data]
 encoder = LabelEncoder()
 encoded_labels = encoder.fit_transform(labels)
@@ -91,26 +98,30 @@ num_classes = len(encoder.classes_)
 encoded_labels = to_categorical(encoded_labels, num_classes=num_classes)
 
 labeled_data_encoded = []
+#combine points with one-hot label
 for i in range(len(labeled_data)):
     point_batches, _ = labeled_data[i]
     encoded_label = encoded_labels[i]
     labeled_data_encoded.append((point_batches, encoded_label))
-    
+
+#test train split for testing
+#used shuffle=False and random_state=17 for re-producable results
 train_data, test_data = train_test_split(labeled_data_encoded, test_size=0.4, random_state=17, shuffle=False)
+#Demo CNN function
 def create_3d_cnn(input_shape, num_classes): 
     model = Sequential([
-        Reshape((100, 10, 1), input_shape=input_shape), 
-        Conv2D(32, (3, 3), padding="same", activation="relu"),
+        Reshape((100, 10, 1), input_shape=input_shape),  #reshape tensor from 500x2x1 to 100x10x1
+        Conv2D(32, (3, 3), padding="same", activation="relu"), #increase Z dim
         BatchNormalization(),
         Dropout(0.5),
-        MaxPooling2D(pool_size=(2, 2)), 
+        MaxPooling2D(pool_size=(2, 2)),  #pool to 50x5x1
         Conv2D(16, (3, 3), padding="same", activation="relu"),
         BatchNormalization(),
         Dropout(0.5),
-        MaxPooling2D(pool_size=(2, 1)),
+        MaxPooling2D(pool_size=(2, 1)),# pool to 25x5x1
         Conv2D(8, (3, 3), padding="same", activation="relu"),
         BatchNormalization(),
-        Flatten(),
+        Flatten(), #start output layers
         Dense(32, activation="relu"),
         Dropout(0.5),
         Dense(num_classes, activation="softmax")
@@ -126,14 +137,14 @@ def train_3d_cnn(train_data, test_data, input_shape, num_classes):
     augmented_test_data = augment_data(test_data)
     train_instances = []
     test_instances = []
-
+    #seperate 5x2x500  batches into 5 2x500 batches
     for batch, label in augmented_train_data:
         for instance in batch:
             train_instances.append((instance, label))
     for batch, label in augmented_test_data:
         for instance in batch:
             test_instances.append((instance, label))
-            
+    #Seperate labels and points     
     train_batches = np.array([instance for instance, _ in train_instances])
     train_labels = np.array([label for _, label in train_instances])
     test_batches = np.array([instance for instance, _ in test_instances])
@@ -142,6 +153,7 @@ def train_3d_cnn(train_data, test_data, input_shape, num_classes):
     # Added early stopping to prevent overfitting
     early_stopping = EarlyStopping(monitor='val_loss', patience=5)
 
+    
     model.fit(train_batches, train_labels, epochs=100, batch_size=64, 
               validation_data=(test_batches, test_labels),
               callbacks=[early_stopping])
